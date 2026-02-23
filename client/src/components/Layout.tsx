3a95f59ddc8ac93976cf43dfe93654f6ecc0f785
edanurbinici <edanur0x0@gmail.com>
2026-02-23 20:02:13 +0300

    ...

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -32,7 +32,7 @@ export const Layout = () => {
             {}
             <Link 
               to="/dashboard" 
-              className="flex items-center space-x-3 group"
+              className="flex items-center space-x-2 group -ml-2"
             >
               <div className="w-10 h-10 bg-gradient-to-br from-blue-600 to-blue-700 rounded-xl flex items-center justify-center shadow-lg group-hover:shadow-xl transition-all duration-200 group-hover:scale-105">
                 <span className="text-white text-xl font-bold">JT</span>
